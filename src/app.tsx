3eef852eb00621dcbd32305a787a0b993fba9798
Icaro Campos <icarocampos97@gmail.com>
2025-07-11 01:50:30 -0300

    feat: integrations and new components

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { CreateRoom } from './pages/create-room';
-import { Room } from './pages/room';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { RecordRoomAudio } from './pages/record-room-audio'
+import { Room } from './pages/room'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export function App() {
   return (
@@ -12,8 +13,9 @@ export function App() {
         <Routes>
           <Route element={<CreateRoom />} index />
           <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  );
+  )
 }
